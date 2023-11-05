1c5ddc155da3be1c6065a7ebe787944db13e0d06
AbuzCoder <abuzcoder@gmail.com>
2023-11-05 15:20:40 +0500

    add pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-import FacebookPixel from "./components/FacebookPixel";
+import Script from "next/script";
 const inter = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Telefon orqali 1000$ daromad topish",
   description: "Doniyor Abduganiyev Telefon orqali 1000$ daromad topish",
-}
+};
 
 export default function RootLayout({
   children,
@@ -18,8 +18,26 @@ export default function RootLayout({
     <html lang="en">
       <head></head>
       <body className={inter.className}>
+        <Script strategy="afterInteractive" id="facebook-pixel">
+          {`!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1226357271369927');
+            fbq('track', 'PageView');
+              `}
+        </Script>
+        <noscript dangerouslySetInnerHTML={{__html: `<img
+            height="1"
+            width="1"
+            style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=1226357271369927&ev=PageView&noscript=1"
+          />`}} />
         {children}
-        <FacebookPixel />
       </body>
     </html>
   );
