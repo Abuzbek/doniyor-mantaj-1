368fefa1f2eb0d9315ff04b2b20e9f1e44fbfc8e
AbuzCoder <abuzcoder@gmail.com>
2023-11-05 15:17:48 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Montserrat({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Telefon orqali 1000$ daromad topish",
   description: "Doniyor Abduganiyev Telefon orqali 1000$ daromad topish",
-};
+}
 
 export default function RootLayout({
   children,
